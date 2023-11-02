af115da12314073b1cea2468afdd0aba64a3cd62
hany0ageeb <hany0ageeb@gmail.com>
2023-11-02 06:44:43 -0700

    adding file 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     if (length != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    operators = ["+", "-", "*", "/"]
+    operators = ("+", "-", "*", "/")
     operator = sys.argv[2]
     if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
