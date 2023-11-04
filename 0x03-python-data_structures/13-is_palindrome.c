c7d6ca262fd856af0d99a6bd9f6598d552d23f82
hany0ageeb <hany0ageeb@gmail.com>
2023-11-04 04:04:56 -0700

    efficieny issue with code in file 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -23,6 +23,25 @@ listint_t *make_it_doubly_linked(listint_t *head)
 	return (v);
 }
 /**
+ * _is_palindrome - recursively check if listint_t is palindrome
+ * @first: first node
+ * @last: last_node
+ * Return: 0 if it is not a palindrome, 1 if it is
+ */
+static int _is_palindrome(listint_t *first, listint_t **last)
+{
+	int ret_code = 0;
+
+	if (first == NULL)
+		return (1);
+	if (_is_palindrome(first->next, last) != 1)
+		return (0);
+	if (first->n == (*last)->n)
+		ret_code = 1;
+	*last = (*last)->next;
+	return (ret_code);
+}
+/**
  * is_palindrome -  checks if a singly linked list is a palindrome.
  * @head: linked list head
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -35,14 +54,5 @@ int is_palindrome(listint_t **head)
 		return (1);
 	if ((*head)->next == NULL)
 		return (1);
-	last = make_it_doubly_linked(*head);
-	first = *head;
-	while (first->previous != last && first != last)
-	{
-		if (first->n != last->n)
-			return (0);
-		first = first->next;
-		last = last->previous;
-	}
-	return (1);
+	return (_is_palindrome(*head, head));
 }
