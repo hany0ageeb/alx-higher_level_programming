95e207a7c2b6b0754c6a9f925d414bde8eebeed1
hany0ageeb <hany0ageeb@gmail.com>
2023-11-04 03:38:24 -0700

    fixing efficiency issue with the code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,27 @@
 #include "lists.h"
 #include <stdio.h>
+
+/**
+ * make_it_doubly_linked - make listint_t a doubly linked list
+ * @head: head node
+ * Return: pointer to tail node
+ */
+listint_t *make_it_doubly_linked(listint_t *head)
+{
+	listint_t *v = NULL;
+
+	if (head != NULL && head->next != NULL)
+	{
+		head->previous = NULL;
+		v = head;
+		while (v->next != NULL)
+		{
+			v->next->previous = v;
+			v = v->next;
+		}
+	}
+	return (v);
+}
 /**
  * is_palindrome -  checks if a singly linked list is a palindrome.
  * @head: linked list head
@@ -7,24 +29,20 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *first, *last, *end = NULL;
+	listint_t *first, *last;
 
 	if (*head == NULL)
 		return (1);
 	if ((*head)->next == NULL)
 		return (1);
+	last = make_it_doubly_linked(*head);
 	first = *head;
-	while (first != end)
+	while (first->previous != last && first != last)
 	{
-		last = first;
-		while (last->next != end)
-			last = last->next;
 		if (first->n != last->n)
 			return (0);
-		if (first == last)
-			break;
-		end = last;
 		first = first->next;
+		last = last->previous;
 	}
 	return (1);
 }
