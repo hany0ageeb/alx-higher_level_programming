da822ffe39729b57d051a2dfb83600350f017840
hany0ageeb <hany0ageeb@gmail.com>
2023-11-04 02:07:57 -0700

    fixing 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,8 @@ def print_matrix_integer(matrix=[[]]):
         if row is None:
             continue
         for j in range(0, len(row)):
-            print("{:d}".format(row[j]), end=' ')
+            if j < len(row) - 1:
+                print("{:d}".format(row[j]), end=' ')
+            else:
+                print("{:d}".format(row[j]), end='')
         print()
