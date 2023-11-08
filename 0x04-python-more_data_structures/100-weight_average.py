6003a4f924f10dfb1cdad01089adf126fb754759
hany0ageeb <hany0ageeb@gmail.com>
2023-11-08 13:47:42 -0800

    adding file 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+def weight_average(my_list=[]):
+    left = 0
+    right = 0
+    for a_tuple in my_list:
+        left += (a_tuple[0] * a_tuple[1])
+        right += a_tuple[1]
+    if right == 0:
+        return 0
+    return left / right
