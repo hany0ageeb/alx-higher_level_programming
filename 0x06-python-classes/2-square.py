306c5fb15b602142aeb251bbfa3872fc53bf412d
hany0ageeb <hany0ageeb@gmail.com>
2023-11-21 03:12:10 -0800

    adding 2-square.py comments to file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""
+    module 2-square which define a simple Square class
+"""
+
+
 class Square:
+    """
+    class Square - represents a simple Square with size
+    Attributes:
+        size(int): square size should be >= 0
+    """
     def __init__(self, size: int = 0):
+        """
+        __init__ function - initialize Square object
+        Args:
+            size(int): the size of the quare
+        """
         if type(size) != int:
             raise TypeError('size must be an integer')
         if size < 0:
