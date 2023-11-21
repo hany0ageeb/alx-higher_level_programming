6bc078ac2d49e08e355f3af5d00a106c0714fe1e
hany0ageeb <hany0ageeb@gmail.com>
2023-11-21 03:02:44 -0800

    adding comments to 1-square.py file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
+"""
+    1-square module defines a simple Square class
+    Square class has a private instance property __size
+"""
+
+
 class Square:
+    """
+        Square class represents Square with size
+
+        Attributes:
+            size(int): the size of quare should be >= 0
+    """
     def __init__(self, size):
+        """
+            this function initizliize Square object with the given size
+            size should be positive integer >= 0
+            but we do not check for that yet
+            Args:
+                size (int): initial size of the Square
+        """
         self.__size = size
