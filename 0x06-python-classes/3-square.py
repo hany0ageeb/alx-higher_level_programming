033062e069a49eafd8719ffa298a6a0a2b4af8b9
hany0ageeb <hany0ageeb@gmail.com>
2023-11-21 03:25:02 -0800

    adding comments to 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
+"""
+    module 3-square:
+        define a simple Square class
+"""
+
+
 class Square:
-    """Square class"""
+    """
+        class Square:
+            a simple Square with size
+        Args:
+            size(int): the size of square >= 0 defaults to 0
+        Attributes:
+            size(int): square size
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size < 0
+    """
     def __init__(self, size: int = 0):
         if type(size) != int:
             raise TypeError('size must be an integer')
@@ -9,5 +25,9 @@ class Square:
         self.__size = size
 
     def area(self) -> int:
-        """claculate area of the square as size * size"""
+        """
+            claculate area of the square as size * size
+            Returns:
+                int: the area of square = size * size
+        """
         return self.__size * self.__size
