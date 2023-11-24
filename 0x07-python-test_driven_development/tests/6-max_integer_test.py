7c9e7cee8b54e3db187dbd1e605d8159c988f9fe
hany0ageeb <hany0ageeb@gmail.com>
2023-11-24 10:45:46 -0800

    Task 5. Max integer - Unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    TestMaxInteger - tests max_integer function
+    """
+    def test_max_integer(self):
+        """
+        tests max_integer edge cases
+        """
+        self.assertIsNone(max_integer([]))
+        with self.assertRaises(TypeError):
+            result = max_integer([1, 2, 3, 'a'])
+        self.assertIsNone(max_integer())
+        self.assertEqual(max_integer("123"), '3')
+        self.assertEqual(max_integer(['a', 'b', 'c']), 'c')
+        with self.assertRaises(TypeError):
+            result = max_integer(1.5)
+        self.assertEqual(max_integer([5, 4, 3, 2]), 5)
+        self.assertEqual(max_integer([1, 2, 5]), 5)
+        self.assertEqual(max_integer([6.5, 1.0, 2.5]), 6.5)
