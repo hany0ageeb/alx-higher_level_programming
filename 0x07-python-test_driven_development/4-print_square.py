b4c07b6d8fa82fa8128a1b8eb47ca7b5d1711850
hany0ageeb <hany0ageeb@gmail.com>
2023-11-24 10:24:23 -0800

    Task 3. Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+4-print_square contains one function  print_square(size)
+"""
+
+
+def print_square(size):
+    """
+    prints a square with the character #
+    """
+    if type(size) is float and size < 0:
+        raise TypeError('size must be an integer')
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
+    if size == 0:
+        print()
