44e10597bcf57dcf2194301ba1ba5f4ad07fd861
hany0ageeb <hany0ageeb@gmail.com>
2023-11-27 07:17:42 -0800

    Task 0.Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+0-rectangle module: This module define one class: Rectangle
+"""
+
 
 class Rectangle:
+    """
+    class Rectangle:
+    simple class represents a rectangle
+    """
     pass
