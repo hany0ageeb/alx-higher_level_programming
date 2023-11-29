40b89cabee5f7671341490a40eda5dcf67c513f7
hany0ageeb <hany0ageeb@gmail.com>
2023-11-28 22:14:13 -0800

    fixing issue in file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,7 +11,7 @@ def add_integer(a, b=98):
     """
     adds 2 integer
     """
-    if type(a) not in (float, int):
+    if a is None or type(a) not in (float, int):
         raise TypeError('a must be an integer')
     if type(b) not in (float, int):
         raise TypeError('b must be an integer')
