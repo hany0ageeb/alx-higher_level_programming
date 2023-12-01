86ba777e14202a26f86fcc3b8b5b129101d32cff
hany0ageeb <hany0ageeb@gmail.com>
2023-12-01 02:14:32 -0800

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,6 +6,7 @@ Module 5-text_indentation which contains func text_indentation(text)
 
 
 def text_indentation(text):
+    line_begin = True
     """
     a function that prints a text with 2 new lines after
     each of these characters: ., ? and :
@@ -13,7 +14,12 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError('text must be a string')
     for ch in text:
-        print(ch, end='')
+        if line_begin and ch != ' ':
+            print(ch, end='')
+            line_begin = False
+        elif not line_begin:
+            print(ch, end='')
         if ch in ('.', '?', ':'):
             print()
             print()
+            line_begin = True
