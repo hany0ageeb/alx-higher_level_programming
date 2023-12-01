515efcfecaf0797d253a2540d494d066a5ab5684
hany0ageeb <hany0ageeb@gmail.com>
2023-12-01 01:57:51 -0800

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -15,9 +15,9 @@ def print_square(size):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
+    if size == 0:
+        return
     for i in range(size):
         for j in range(size):
             print('#', end='')
-        print()
-    if size == 0:
         print()
