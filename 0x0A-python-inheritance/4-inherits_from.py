3af7c783fbd732cca57cd3892e75ba6b6bcffb2a
hany0ageeb <hany0ageeb@gmail.com>
2023-12-04 02:30:34 -0800

    Task 4.Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,5 @@
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class ; otherwise False"""
-    return issubclass(type(obj), a_class)
+
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
