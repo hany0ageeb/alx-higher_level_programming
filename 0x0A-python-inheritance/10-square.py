b148007dd04d87df30401455490eec10f5fc6a42
hany0ageeb <hany0ageeb@gmail.com>
2023-12-04 03:52:54 -0800

    Task 10.Square#1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a single class: Square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangel"""
+    def __init__(self, size):
+        """initialize a square object
+        Args:
+            size(int): square size must be int > 0
+        """
+        super().__init__(size, size)
