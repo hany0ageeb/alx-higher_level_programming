86e3211ba518334b9c7cc207ee2c9d92b8fe268f
hany0ageeb <hany0ageeb@gmail.com>
2023-12-04 03:06:00 -0800

    increasing docstring length!

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,13 +3,26 @@
 
 
 class BaseGeometry:
-    """ empty class BaseGeometry"""
+    """class BaseGeometry
+    Instance Methods:
+        area
+        integer_validator
+    """
     def area(self):
-        """raises an Exception"""
+        """raises an Exception with message: areas() is not implemented"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """validates value"""
+        """validates value
+        Args:
+            name(str): name
+            value(int): value to validate
+        Raises:
+            TypeError: if value is not int
+            ValueError: if value is <= 0
+        Returns:
+            None: if validation passed
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
