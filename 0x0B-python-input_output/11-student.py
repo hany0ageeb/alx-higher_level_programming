53777c8073e1bc33e2fd63589c990de3906a6553
hany0ageeb <hany0ageeb@gmail.com>
2023-12-05 01:54:53 -0800

    Task 11.Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module defines a single class: Student
+"""
+
+
+class Student:
+    """class Student
+    Attributes:
+        first_name(str): (Instance attribute) student first name
+        last_name(str): (Instance attribute) student last name
+        age(int): (Instance attribute) student age
+    Args:
+        first_name(str): first name
+        last_name(str): last name
+        age(int): age
+    """
+    def __init__(self, first_name: str, last_name: str, age: int):
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+        self.age: int = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance
+        args:
+            attrs(list of str): attributes to retrive
+        """
+        if attrs is None:
+            return dict(self.__dict__)
+        tmp = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                tmp[key] = value
+        return tmp
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance"""
+        for key, value in json:
+            if key in self.__dict__:
+                self.__dict__[key] = value
