8d167ab809410ac283e806616b21518615ce5060
hany0ageeb <hany0ageeb@gmail.com>
2023-12-05 23:32:56 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -12,6 +12,7 @@ class Metrics:
     def __init__(self):
         self.total_file_size = 0
         self.status_codes = {}
+        self.is_printed = True
 
     def log_line(self, line):
         """compute and update metrices"""
@@ -26,13 +27,17 @@ class Metrics:
         else:
             self.total_file_size += int(file_size)
         self.status_codes[status] = self.status_codes.get(status, 0) + 1
+        self.is_printed = False
 
     def print_metrics(self):
         """print metrices with specified format"""
+        if self.is_printed:
+            return
         msg = "File size: {:d}\n".format(self.total_file_size)
         for key, value in sorted(self.status_codes.items()):
             msg += "{}: {:d}\n".format(key, value)
         print(msg, end='')
+        self.is_printed = True
 
 
 def main():
@@ -46,6 +51,7 @@ def main():
             line_counter += 1
             if line_counter % 10 == 0:
                 m.print_metrics()
+        m.print_metrics()
     except KeyboardInterrupt as e:
         m.print_metrics()
         raise
