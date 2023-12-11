0fc1f29d6b7069a4e8b4929fd2eea8a3251a825d
hany0ageeb <hany0ageeb@gmail.com>
2023-12-11 07:33:48 -0800

    fixing issues

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -60,9 +60,10 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        dummy = cls(1, 1)
-        dummy.update(**dictionary)
-        return dummy
+        if issubclass(cls, Base):
+            dummy = cls(1, 1)
+            dummy.update(**dictionary)
+            return dummy
 
     @classmethod
     def load_from_file(cls):
