8b824cdeecbfa98608f63ae50a89c342f5378cce
hany0ageeb <hany0ageeb@gmail.com>
2024-01-17 04:39:22 -0800

    16. List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
-SELECT `tv_shows`.`title` AS "Title",
+SELECT `tv_shows`.`title` AS "title",
 	`tv_genres`.`name` AS "name"
 FROM `tv_shows`
 LEFT OUTER JOIN `tv_show_genres`
