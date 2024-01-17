ada25773ad1b097b2d45c029123c203d846ddeb9
hany0ageeb <hany0ageeb@gmail.com>
2024-01-17 04:38:28 -0800

    16. List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,7 +4,7 @@ SELECT `tv_shows`.`title` AS "Title",
 FROM `tv_shows`
 LEFT OUTER JOIN `tv_show_genres`
 	ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
-LEFT OUTER JOIn `tv_genres`
+LEFT OUTER JOIN `tv_genres`
 	ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
 ORDER BY `tv_shows`.`title`,
 	 `tv_genres`.`name`;
