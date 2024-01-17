e6d3c7b84f802e31d0df020bb1b73df85f7ff59b
hany0ageeb <hany0ageeb@gmail.com>
2024-01-17 10:52:57 -0800

    Task 18. No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,10 @@
--- a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
-SELECT DISTINCT tv_shows.title AS "title"
-FROM tv_shows
-LEFT OUTER JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-OR tv_show_genres.genre_id <> (
-	SELECT tv_genres.id
-	FROM tv_genres
-	WHERE tv_genres.name = "Comedy")
-ORDER BY tv_shows.title;
+-- a script that lists all shows without the genre Comedy in the database hbtn_0d_tvs
+SELECT `tv_shows`.`title` AS title
+FROM `tv_shows`
+WHERE `tv_shows`.`id` NOT IN (
+	SELECT `tv_show_genres`.`show_id`
+	FROM `tv_show_genres`
+	INNER JOIN `tv_genres`
+		ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
+	WHERE `tv_genres`.`name` = "Comedy")
+ORDER BY `tv_shows`.`title`;
