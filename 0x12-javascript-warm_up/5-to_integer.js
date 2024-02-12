929f7be442852342784a065135989dceb8a1146a
hany0ageeb <hany0ageeb@gmail.com>
2024-02-12 03:41:42 -0800

    Task 5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const arg = argv.length > 2 ? argv[2] : undefined;
+const arg = process.argv.length > 2 ? parseInt(process.argv[2]) : undefined;
 if (arg && !Number.isNaN(arg)) {
-  console.log(`My number: ${parseInt(arg)}`);
+  console.log(`My number: ${arg}`);
 } else {
   console.log('Not a number');
 }
