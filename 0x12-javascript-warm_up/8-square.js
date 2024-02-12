cbc0f31717f894cf5a9f3be74d99c9b6306daa0f
hany0ageeb <hany0ageeb@gmail.com>
2024-02-12 03:46:44 -0800

    Task 8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const size = argv.length === 3 ? parseInt(argv[2]) : undefined;
+const size = process.argv.length === 3 ? parseInt(process.argv[2]) : undefined;
 if (size && !Number.isNaN(size)) {
   if (size > 0) {
     for (let i = 0; i < size; ++i) {
