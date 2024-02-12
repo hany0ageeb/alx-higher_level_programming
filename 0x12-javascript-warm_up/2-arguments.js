c648788261d55d8042dcddf9ba2280ea9c59e50e
hany0ageeb <hany0ageeb@gmail.com>
2024-02-12 02:46:12 -0800

    Task 2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-const args = argv.slice(2);
-if (args.length === 0) {
+if (argv.length <= 2) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (argv.length <= 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
