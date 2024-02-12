57839a5dfdc90f4101bd26bd58789a290366f632
hany0ageeb <hany0ageeb@gmail.com>
2024-02-12 04:37:47 -0800

    Task 11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const nums = process.argv.slice(2).filter((x) => x && !Number.isNaN(x)).map((x) => parseInt(x));
+const nums = process.argv.slice(2).map((x) => parseInt(x));
 if (nums.length >= 2) {
   nums.sort();
   console.log(nums[nums.length - 2]);
