21e716862868a8dbcedf4e09bf1747590b17fbb8
hany0ageeb <hany0ageeb@gmail.com>
2024-02-12 03:49:48 -0800

    Task 9. Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,6 @@
 function add (a, b) {
   return a + b;
 }
-const { argv } = require('node:process');
-const a = argv.length > 2 ? parseInt(argv[2]) : Number.NaN;
-const b = argv.length > 3 ? parseInt(argv[3]) : Number.NaN;
+const a = process.argv.length > 2 ? parseInt(process.argv[2]) : Number.NaN;
+const b = process.argv.length > 3 ? parseInt(process.argv[3]) : Number.NaN;
 console.log(add(a, b));
