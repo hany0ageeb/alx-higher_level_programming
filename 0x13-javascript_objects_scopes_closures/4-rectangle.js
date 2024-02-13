85a5f5fab245448562cf9db716522710a5cdfb50
hany0ageeb <hany0ageeb@gmail.com>
2024-02-13 03:56:35 -0800

    Task 4. Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,44 +1,26 @@
 #!/usr/bin/node
 class Rectangle {
-  #width;
-  #height;
   constructor (w, h) {
     if (w > 0 && w === parseInt(w) && h > 0 && h === parseInt(h)) {
-      this.#width = w;
-      this.#height = h;
+      this.width = w;
+      this.height = h;
     }
   }
 
-  get width () {
-    return this.#width;
-  }
-
-  set width (w) {
-    this.#width = w;
-  }
-
-  get height () {
-    return this.#height;
-  }
-
-  set height (h) {
-    this.#height = h;
-  }
-
   print () {
-    const row = 'X'.repeat(this.#width);
-    for (let i = 0; i < this.#height; ++i) {
+    const row = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; ++i) {
       console.log(row);
     }
   }
 
   rotate () {
-    [this.#width, this.#height] = [this.#height, this.#width];
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
-    this.#width *= 2;
-    this.#height *= 2;
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 module.exports = Rectangle;
