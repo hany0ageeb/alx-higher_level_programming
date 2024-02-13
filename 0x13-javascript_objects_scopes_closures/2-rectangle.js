dbff41edcd10b5ef3b40b8646c129be23bb4d00a
hany0ageeb <hany0ageeb@gmail.com>
2024-02-13 03:38:24 -0800

    Task 2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w && w > 0 && h && h > 0) {
+    if (w > 0 && h > 0 && w === parseInt(w) && h === parseInt(h)) {
       this.width = w;
       this.height = h;
-    } else {
-      return {};
     }
   }
 }
