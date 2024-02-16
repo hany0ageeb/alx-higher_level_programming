45b8b69ee1a35858f333c47715f750cf7cea7cda
hany0ageeb <hany0ageeb@gmail.com>
2024-02-15 23:43:47 -0800

    Task 1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-1. Your script should take 4 arguments:
-    mysql username, mysql password, database name, and state name searched
-2. You must use the module MySQLdb
-3. Your script should connect to a MySQL
-   server running on localhost at port 3306
-4. You must use format to create the SQL query with the user input
 """
+script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+1. Your script should take 3 arguments:
+    mysql username, mysql password and database name
+2. You must use the module MySQLdb
+3. Your script should connect to a MySQL server
+    running on localhost at port 3306
+4. Results must be sorted in ascending order by states.id
+5. Results must be displayed as they are in the example below
+6. Your code should not be executed when imported
+Output Example:
+    (4, 'New York')
+    (5, 'Nevada')
+"""
+import MySQLdb
+import sys
 
 
 def main():
-    """script Entry Point
     """
-    import sys
-    import MySQLdb
-    USER_NAME = sys.argv[1]
-    PASSWORD = sys.argv[2]
-    DB_NAME = sys.argv[3]
-    HOST = "localhost"
-    PORT = 3306
-    STATE_NAME = "N%"
-
-    db = MySQLdb.connect(
-            host=HOST,
-            port=PORT,
-            database=DB_NAME,
-            user=USER_NAME,
-            password=PASSWORD)
-    qry = """
-    SELECT id, name
-    FROM states
-    WHERE name LIKE '{}'
-    ORDER BY id""".format(STATE_NAME)
-    db.query(qry)
-    result = db.store_result()
-    for row in result.fetch_row(maxrows=0):
+    script Entry Point
+    """
+    conn = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("""
+            SELECT states.id, states.name
+            FROM states
+            WHERE states.name LIKE %s
+            ORDER BY states.id""", ('N%',))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-    db.close()
+    cur.close()
+    conn.close()
 
 
 if __name__ == '__main__':
