ddf68373caee74de9d67aeb4263bef90f18d6cb7
hany0ageeb <hany0ageeb@gmail.com>
2024-02-29 03:21:20 -0800

    Task 7. Only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl --silent --head --write-out "%{http_code}\n" -o /dev/null "$1"
+curl --silent --head --write-out "%{http_code}" -o /dev/null "$1"
