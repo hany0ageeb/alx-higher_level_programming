bfb4d97cb513bd8cff2b3443c0b977536093046a
hany0ageeb <hany0ageeb@gmail.com>
2024-02-29 03:36:12 -0800

    Task 9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl --silent --request POST 0.0.0.0:5000/catch_me
+curl --silent --request DELETE 0.0.0.0:5000/catch_me
