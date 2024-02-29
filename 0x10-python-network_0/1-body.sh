e6d1a4f04a11424ab240d1fb3b68dda20162fb3f
hany0ageeb <hany0ageeb@gmail.com>
2024-02-29 01:57:13 -0800

    Task 1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,7 +2,7 @@
 #  Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 # 1. Display only body of a 200 status code response
 # 2. You have to use curl
-status_code="$(curl --silent --head "$1" | awk '/^HTTP/{print $2}')"
+status_code="$(curl -s --head "$1" | awk '/^HTTP/{print $2}')"
 if [[ "$status_code" == "200" ]]; then
-	curl --silent "$1"
+	curl -s "$1"
 fi
