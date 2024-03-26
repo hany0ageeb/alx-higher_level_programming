aca4af1f916c9acd9efa2e8f9ba1c7766ef6029e
hany0ageeb <hany0ageeb@gmail.com>
2024-03-26 02:48:35 -0700

    Task 4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -13,11 +13,14 @@ request.get(process.argv[2], function (error, response, body) {
   }
   if (response && response.statusCode === 200 && body) {
     const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
-    const films = JSON.parse(body).results.filter((film) => film.characters.includes(characterUrl));
-    if (films) {
-      console.log(films.length);
-    } else {
-      console.log(0);
+    const r = JSON.parse(body);
+    if (r && r.results) {
+      const films = JSON.parse(body).results.filter((film) => film.characters && film.characters.includes(characterUrl));
+      if (films) {
+        console.log(films.length);
+      } else {
+        console.log(0);
+      }
     }
   }
 });
