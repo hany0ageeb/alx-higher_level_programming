6a309a09952f5fa2c271d40d52117ff8815b53dc
hany0ageeb <hany0ageeb@gmail.com>
2024-03-26 04:16:33 -0700

    Task 8. Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 /**
- * script that prints all characters of a Star Wars movie:
- *  1. The first argument is the Movie ID - example: 3 = “Return of the Jedi”
- *  2. Display one character name by line in the same order of the list “characters” in the /films/ response
- *  3. You must use the Star wars API
- *  4. You must use the module request
+ * script that prints all characters of a Star Wars movie
+ * 1. The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+ * 2. Display one character name by line
+ * 3. You must use the Star wars API
+ * 4. You must use the module request
  */
 
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-  if (response && response.statusCode === 200 && body) {
+  if (!error && response && response.statusCode === 200 && body) {
     JSON.parse(body).characters.forEach((characterUrl) => {
-      request.get(characterUrl, (error1, response1, body1) => {
-        if (error1) {
-          console.log(error1);
-        }
-        if (response1 && response1.statusCode === 200 && body1) {
-          const character = JSON.parse(body1);
-          console.log(character.name);
+      request.get(characterUrl, (error2, response2, body2) => {
+        if (!error2 && response2 && response2.statusCode === 200 && body2) {
+          console.log(JSON.parse(body2).name);
         }
       });
     });
